a941f1a888a128a184eef3ec53ac0fae77855146
hyunwoo00 <khwland090@gmail.com>
2022-08-19 19:46:10 +0900

    js base study

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-alert("hi");
+function sayhello(nameofperson, age) {
+  console.log("Hi! my name is " + nameofperson + " and I am " + age);
+}
+
+sayhello();
+sayhello("khw");
+sayhello("khw", 23);
+
+function plus(firstnum, secondnum) {
+  console.log(firstnum + secondnum);
+}
+
+// console.log(firstnum, secondnum); // 에러
+
+plus(60, 8);
+
+function divide(firstnum, secondnum) {
+  console.log(firstnum / secondnum);
+}
+
+divide(60, 8);
+
+const player = {
+  name: "khw",
+  sayhello: function (othername) {
+    console.log("hello! " + othername);
+  },
+};
+
+console.log(player.name);
+player.sayhello("lyn");
+player.sayhello("nico");
