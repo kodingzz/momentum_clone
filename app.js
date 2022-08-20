990d3ca4a502912ad542e6018aea0aa69f73d92c
hyunwoo00 <khwland090@gmail.com>
2022-08-20 13:39:13 +0900

    Conditionals part Two

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const age = 23;
+const age = parseInt(prompt("How old are you?"));
 
-function calculatorkrage(ageofForeigner) {
-  return ageofForeigner + 2;
+// console.log(isNaN(age));
+
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else {
+  console.log(age);
 }
-
-const krage = calculatorkrage(age);
-
-console.log(krage);
