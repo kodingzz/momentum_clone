4da3663a21b90454d6417883e0fecbd2d25f120f
hyunwoo00 <khwland090@gmail.com>
2022-08-20 15:56:30 +0900

    HTML in Javascript

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title"); //1st
 
-// console.log(isNaN(age));
+console.dir(title);
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a positive number ><");
-} else if (age < 18) {
-  console.log("You are too young :]");
-} else if (age < 50) {
-  console.log("You can drink!");
-} else if (age >= 50 && age < 80) {
-  console.log("You should exercise!");
-} else {
-  console.log("You are too old :(");
-}
+title.innerText = "So good!";
+
+console.log(title.id);
+console.log(title.innerText);
