9d92220d55baa5ddf42f906de32396244aec1f52
hyunwoo00 <khwland090@gmail.com>
2022-08-21 17:41:20 +0900

    CSS in Javascript3

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const title = document.querySelector(".hello h1");
 
-console.dir(title); // console.log(title);
-
 function handleTitleClick() {
-  const currentcolor = title.style.color;
-  let newcolor;
-  if (currentcolor === "blue") {
-    newcolor = "tomato";
-  } else {
-    newcolor = "blue";
-  }
-  title.style.color = newcolor;
-  console.log(newcolor);
+  title.classList.toggle("clicked");
 }
 
 title.addEventListener("click", handleTitleClick);
