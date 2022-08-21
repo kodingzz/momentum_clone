45a94e5cc4b02fe8c61ce225ad81ee3e4e24c4d1
hyunwoo00 <khwland090@gmail.com>
2022-08-21 15:23:30 +0900

    CSS in Javascript1

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 const title = document.querySelector(".hello h1");
 
-console.dir(title);
+console.dir(title); // console.log(title);
 
 function handleTitleClick() {
-  console.log("Title was clicked");
-  title.style.color = "blue";
-}
-function handleMouseEnter() {
-  title.innerText = "Mouse is come!";
-}
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("copier");
+  const currentcolor = title.style.color;
+  let newcolor;
+  if (currentcolor === "blue") {
+    newcolor = "tomato";
+  } else {
+    newcolor = "blue";
+  }
+  title.style.color = newcolor;
+  console.log(newcolor);
 }
 
-function handleWindowOffline() {
-  alert("No Wifi");
-}
-function handleWindowOnline() {
-  alert("Wifi is here!");
-}
 title.addEventListener("click", handleTitleClick);
-
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize); // 화면크기 변경시
-window.addEventListener("copy", handleWindowCopy); // 사용자가 ctrl+c 하면 alert(경고문)
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
