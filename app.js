b56a72812ea451c71f4712ec7fff679a29c9fbbd
hyunwoo00 <khwland090@gmail.com>
2022-08-21 18:11:19 +0900

    Form Submission1

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const logininput = document.querySelector("#login-form input"); // login-form id
 const loginbutton = document.querySelector("#login-form button"); // login-form id안의 button
 
 function onLoginButton() {
-  console.log("hello " + logininput.value); // value는 input의 입력값
+  const username = logininput.value;
+  if (username === "") {
+    alert("Please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long!");
+  } else {
+    console.log("hello " + logininput.value); // value는 input의 입력값
+  }
 }
+
 loginbutton.addEventListener("click", onLoginButton);
