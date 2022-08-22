a7b91df36e928d9f225298ec04a09e5701c3c35c
hyunwoo00 <khwland090@gmail.com>
2022-08-22 13:20:48 +0900

    Loading username

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logininput = document.querySelector("#login-form input"); // login-form id
 const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   //const username = logininput.value;
@@ -10,14 +11,22 @@ function onLoginSubmit(event) {
   loginform.classList.add("hidden"); // form을 제거해주는 hidden클래스 추가
   const username = logininput.value;
   console.log(username);
+  paintgreetings(username);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function paintgreetings(username) {
+  // 두 문장이상이 겹치는 경우 함수로 만들어서 사용하는게 편함
   greeting.innerText = `Hello ${username}`; //greeting.innerText = "Hello " + username;
-  greeting.classList.remove("hidden"); //클래스를 추가하거나 삭제할때 , classList.add, remo e
-  localStorage.setItem("username", username);
+  greeting.classList.remove("hidden"); //클래스를 추가하거나 삭제할때 , classList.add, remove
 }
 
-function LinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+if (savedUsername === null) {
+  console.log(savedUsername);
+  loginform.classList.remove("hidden");
+  loginform.addEventListener("submit", onLoginSubmit);
+} else {
+  console.log(savedUsername);
+  paintgreetings(savedUsername);
 }
-
-loginform.addEventListener("submit", onLoginSubmit);
