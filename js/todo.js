70f31ee6dc2ecb5551a0c35e8a6b3dbbeaa64f22
hyunwoo00 <khwland090@gmail.com>
2022-08-22 22:35:17 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -15,10 +15,22 @@ function PaintTodo(newtodo) {
   // input에 적은 값을 list에 추가해주는 함수
   const li = document.createElement("li"); // li 태그 생성
   const span = document.createElement("span"); // span 태그 생성
-  li.appendChild(span); // li 안에 span을 넣는다.
+
   span.innerText = newtodo; // span안에 input값을 넣는다.
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", DeleteTodo);
+  li.appendChild(span); // li 안에 span을 넣는다.
+  li.appendChild(button);
+
   //console.log(li);
   todoList.appendChild(li); // todo List에 list item들을 추가한다.
+}
+
+function DeleteTodo(event) {
+  // console.dir(event.target.parentElement);
+  const li = event.target.parentElement;
+  li.remove();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
