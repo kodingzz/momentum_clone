527778152ccfee54dbe77524a1b5b96d833a2c2f
hyunwoo00 <khwland090@gmail.com>
2022-08-22 22:49:54 +0900

    Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 // const todoInput =document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
+const todoS = [];
 
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newtodo = todoInput.value;
   todoInput.value = ""; // input value값이 비워지기전에 newtodo에 값이 저장됨.
   console.log(newtodo);
-  PaintTodo(newtodo);
+  todoS.push(newtodo); //  리스트에 입력값을 추가한다.
+  PaintTodo(newtodo); // 입력값을 화면상에 그린다.(paint)
+  SaveTodos(); //  입력값을  local storage에 저장한다.
+}
+
+function SaveTodos() {
+  localStorage.setItem("toDos", JSON.stringify(todoS)); // localstorage에  저장할때 배열의 형태로 저장(덮어쓰기 가능)
 }
 
 function PaintTodo(newtodo) {
@@ -29,8 +36,8 @@ function PaintTodo(newtodo) {
 
 function DeleteTodo(event) {
   // console.dir(event.target.parentElement);
-  const li = event.target.parentElement;
-  li.remove();
+  const li = event.target.parentElement; // target 은 button이다. button의 부모는 li
+  li.remove(); // 클릭된 button의 부모 li를 제거하면 버튼이 제거된다. +text도 제거
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
