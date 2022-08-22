218106251e70a74a9f5e394bc423471e357cdd3a
hyunwoo00 <khwland090@gmail.com>
2022-08-22 21:46:42 +0900

    Adding todolist

--- js/todo.js
+++ js/todo.js
@@ -8,6 +8,17 @@ function handleTodoSubmit(event) {
   const newtodo = todoInput.value;
   todoInput.value = ""; // input value값이 비워지기전에 newtodo에 값이 저장됨.
   console.log(newtodo);
+  PaintTodo(newtodo);
+}
+
+function PaintTodo(newtodo) {
+  // input에 적은 값을 list에 추가해주는 함수
+  const li = document.createElement("li"); // li 태그 생성
+  const span = document.createElement("span"); // span 태그 생성
+  li.appendChild(span); // li 안에 span을 넣는다.
+  span.innerText = newtodo; // span안에 input값을 넣는다.
+  //console.log(li);
+  todoList.appendChild(li); // todo List에 list item들을 추가한다.
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
