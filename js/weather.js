097192d46806ee71b88100947c198a10483eba6e
hyunwoo00 <khwland090@gmail.com>
2022-08-23 16:22:30 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,24 @@
+const API_KEY = "5108cbfd28af500f73626a15d80ce50e"; // openweathermap api key
 function onGeoOk(position) {
   // 사용자위치 알아냈을때 함수
   console.log(position);
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in ", lat, lng);
+  const lon = position.coords.longitude;
+  console.log("You live in ", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then(
+    (
+      response // 더 자세히 알고싶으면 백엔드 js를 들어야함
+    ) =>
+      response.json().then((data) => {
+        // data에 내 현재위치의 모든것이 들어있다.
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weather.innerText = data.weather[0].main; // 현재 내위치의 날씨정보 가져오기
+        city.innerText = data.name; // 현재 내 위치의 도시이름 가져오기
+      })
+  );
 }
 
 function onGeoError() {
